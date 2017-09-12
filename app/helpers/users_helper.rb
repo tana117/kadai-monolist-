adbd80d55a9b0ff8db0a378f666f00c824a973d5
Hirofumi Tanahashi <hiro.tanahashi117@gmail.com>
2017-09-12 12:07:35 +0000

    user login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravater_url(user, options = { size: 80 })
-    gravater_id = Digest::MD5::hexdigest(user.email.downcase)
+  def gravatar_url(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    "https://secure.gravater.com/avater/#{gravater_id}?s=#{size}&d=mm"
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
   end
 end
